ded426f7673ee2fed4e747fdc2cdc57f6753401d
Stanislav <56192405+Satinari@users.noreply.github.com>
2019-11-15 15:27:29 +0200

    add something in Field

---
+++ src/com/company/Values.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Values {
+    private char x = 'X';
+    private char o = 'O';
+    private char n = '_';
+    private char w = '|';
+
+    public void String(){
+
+    }
+}
