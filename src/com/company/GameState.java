553a8851fe063179a9a3b6c382f6d0c7884dc597
Stanislav <56192405+Satinari@users.noreply.github.com>
2019-12-19 16:47:55 +0200

    changes in GameState

--- src/com/company/GameState.java
+++ src/com/company/GameState.java
@@ -6,15 +6,28 @@ public class GameState {
 
     Moves moves = new Moves();
 
-    public void stateOfGame(char value, char[][] array){ 
-        if  ((array[0][0] == value && array[0][1]  == value && array[0][2] == value) ||
-             (array[1][0] == value && array[1][1]  == value && array[1][2] == value) ||
-             (array[2][0] == value && array[2][1]  == value && array[2][2] == value)){
-            if (value == moves.x) {
-                System.out.println(win);
+    public void stateOfGame(char value, char[][] array){
+        
+        for (int i = 0; i < 3; i++) {
+            if (array[i][0] == array[i][1] && array[i][1] == array[i][2] && array[i][0] != '_'){
+                if (value == moves.x) {
+                    System.out.println(win);
+                }
+                else {
+                    System.out.println(lose);
+                }
             }
-            else {
-                System.out.println(lose);
+            System.exit(0);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (array[0][i] == array[1][i] && array[1][i] == array[2][i] && array[0][i] != '_'){
+                if (value == moves.x) {
+                    System.out.println(win);
+                }
+                else {
+                    System.out.println(lose);
+                }
             }
             System.exit(0);
         }
@@ -30,16 +43,6 @@ public class GameState {
             System.exit(0);
         }
 
-        if  ((array[0][0] == value && array[1][0]  == value && array[2][0] == value) ||
-             (array[0][1] == value && array[1][1]  == value && array[2][1] == value) ||
-             (array[0][2] == value && array[1][2]  == value && array[2][2] == value)){
-            if (value == moves.x) {
-                System.out.println(win);
-            }
-            else {
-                System.out.println(lose);
-            }
-            System.exit(0);
-        }
+        
     }
 }
